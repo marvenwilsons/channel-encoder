6229df8e4b96ea4baeffbdddd2ecc12a0d2e671f
Marven <marvenwilsons@gmail.com>
2021-04-09 12:50:41 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Usage
+```html
+<template>
+    <main>
+        <channelEncoder 
+            @onChange="changeHanlder"
+            :data="myData"
+            :config="myConfig"
+        />
+    </main>
+</template>
+
+<script>
+export default {
+    data: () => ({
+        myData: { // default values
+            
+        },
+        myConfig: {
+            fields: ['X Position', 'Y Position', 'Size', 'Luminance'], // the object keys, array of strings
+            options: ['Categorical', 'Orderred', 'Quantitative'], // the possible value of those keys, array of strings or number
+            maxWidth: '200px', // default 300px
+            borderColor: 'red',
+            textColor: 'green',
+            backgroundColor: '#333',
+            hoveredBackground: 'green'
+        }
+    }),
+    methods: {
+        changeHanlder(data) {
+            // fires every data change
+        }
+    }
+}
+</script>
+```
---
+++ index.vue
@@ -0,0 +1,198 @@
+<template>
+    <!-- this component is useful when you have multiple fields with the same set of options  -->
+    <main class="flex" >
+        <section role="wrapper" id="wierd-table-inp" :style="{maxWidth, border: `1px solid ${borderColor}`, background: backgroundColor}" >
+            <section style="z-index: -1" class="flex flexend" >
+                <section   >
+                    <div id="multi-opt" class="flex flexcol" >
+                        <div v-for="(option,option_index) in options" :key="`${option}_${option_index}_${option_index * 2}`" 
+                            :style="{borderRight: `1px solid ${borderColor}`, color: textColor}" 
+                            class="opt-item" 
+                            :id="option"
+                            >
+                            <small>{{option}}</small>
+                        </div>
+                    </div>
+                </section>
+            </section>
+            <section style="z-index: 100;" class="overflowhidden" >
+                <div :style="{borderTop: `1px solid ${borderColor}`, background: backgroundColor}" 
+                    v-for="(field, field_index) in fields" :key="`${field}_${field_index}`" 
+                    class="row" >
+                    <!-- label -->
+                    <div :id="field" class="label padleft050" :style="{color: textColor}" > <small>{{field}}</small> </div>
+                    <!-- blocks -->
+                    <div @click="registerSelection(field,option)"
+                        @mouseenter="handleMouseActivity(true,`${field}-${option_index}`,`${option}-${field_index}`)"
+                        @mouseleave="handleMouseActivity(false,`${field}-${option_index}`,`${option}-${field_index}`)"
+                        class="ch-inp flex flexcenter" v-for="(option,option_index) in options" 
+                        :style="{borderLeft: `1px solid ${borderColor}`}"
+                        :key="`${option}_${option_index}`" 
+                        :data-field="`${field}-${option_index}`"
+                        :data-option="`${option}-${field_index}`"
+                        >
+                        <div v-if="ready" :style="{color: textColor}" >
+                            <span v-if="value[field] == option" >
+                                ✱
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </section>
+    </main>
+</template>
+
+<script>
+export default{
+    props: ["data", "_key", "color", "key_index", "disabled", "config"],
+    data: () => ({
+        fields: undefined,
+        options: undefined,
+        value: {},
+        ready: false,
+        maxWidth: '300px',
+        markedColor: 'gray',
+        borderColor: 'lightgray',
+        backgroundColor: 'white',
+        textColor: '#333',
+        hoveredBackground: 'rgba(211, 211, 211, 0.384)'
+    }),
+    methods: {
+        disableCell() {
+
+        },
+        handleMouseActivity(isHovered,fieldName,optionName) {
+            const lightUpHorizontal = (el) => {
+                const selectedFieldElements = document.querySelectorAll(`[data-field='${el}']`)
+
+                if(isHovered) {
+                    // apply vertical style hover
+                    const fId = fieldName.split('-')[0]
+                    const OId = optionName.split('-')[0]
+                    document.getElementById(fId).style.background = this.hoveredBackground
+                    document.getElementById(OId).style.background = this.hoveredBackground
+                    for(let i = 0; i < selectedFieldElements.length; i++) {
+                        selectedFieldElements[i].style.background = this.hoveredBackground
+                    }
+
+                } else {
+                    const fId = fieldName.split('-')[0]
+                    const OId = optionName.split('-')[0]
+                    document.getElementById(OId).style.background = 'none'
+                    document.getElementById(fId).style.background = 'none'
+                    for(let i = 0; i < selectedFieldElements.length; i++) {
+                        selectedFieldElements[i].style.background = 'none'
+                    }
+                }
+            }
+
+            const lightUpVerctical = (el) => {
+                const selectedFieldElements = document.querySelectorAll(`[data-option='${el}']`)
+
+                if(isHovered) {
+                    // apply vertical style hover
+                    for(let i = 0; i < selectedFieldElements.length; i++) {
+                        selectedFieldElements[i].style.background = this.hoveredBackground
+                    }
+
+                } else {
+                    for(let i = 0; i < selectedFieldElements.length; i++) {
+                        selectedFieldElements[i].style.background = 'none'
+                    }
+                }
+            }
+
+            //
+            const max = parseInt(fieldName.split('-')[fieldName.split('-').length - 1])
+            for(let i = max; i >= 0; i--) {
+                const fields = fieldName.split('-')[0]
+                lightUpHorizontal(`${fields}-${i}`)
+            }
+
+            const optionMax = parseInt(optionName.split('-')[optionName.split('-').length - 1])
+            for(let i = optionMax; i >= 0; i--) {
+                const option = optionName.split('-')[0]
+                lightUpVerctical(`${option}-${i}`)
+            }
+        },
+        registerSelection(fieldName,optionName) {
+            if(this.value[fieldName] != optionName) {
+                this.value[fieldName] = optionName
+            } else {
+                this.value[fieldName] = undefined
+            }
+            
+            this.ready = false
+
+            setTimeout(() => {
+                this.$emit('onChange',{data: this.value})
+                this.ready = true
+            }, 1);
+        }
+    },
+    mounted() {
+        this.ready = false
+
+        if(this.config) {
+            this.maxWidth = this.config.maxWidth
+            this.config.fields  && (this.fields = this.config.fields)
+            this.config.options && (this.options = this.config.options)
+            this.config.borderColor && (this.borderColor = this.config.borderColor)
+            this.config.textColor && (this.textColor = this.config.textColor)
+            this.config.backgroundColor && (this.backgroundColor = this.config.backgroundColor)
+            this.config.hoveredBackground && (this.hoveredBackground = this.config.hoveredBackground)
+        }
+        
+        this.fields.map(e => {
+            this.value[e] = undefined
+        })
+
+        if(this.data) {
+            this.ready = true
+            this.value = this.data
+            this.$emit('onMount', this.value)
+        }
+    }
+}
+</script>
+
+<style>
+.row {
+    display: flex;
+    z-index: 5;
+    background: white;
+    overflow: hidden !important;
+}
+.ch-inp {
+    min-height:30px;
+    min-width:30px;
+}
+.ch-inp:hover {
+    cursor: pointer;
+}
+.label {
+    flex: 1;
+    display: flex;
+    align-items: center;
+    padding-right: 5px;
+}
+.label > * {
+    width: 100%;
+    text-align: right;
+    
+}
+#multi-opt {
+    writing-mode: vertical-rl;
+    height: 100px !important;
+    width: 100%;
+    flex-direction: column-reverse;
+}
+.opt-item {
+    transform: rotate(-180deg);
+    width: 30px !important;
+    text-align: left;
+    padding-top: 5px;
+    writing-mode: horizontal-lr
+}
+</style>
